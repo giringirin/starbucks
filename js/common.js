33272b45c0c13fe56e77e5246385cf3a731f0521
ginnal <ginnal0224@gmail.com>
2023-12-13 11:34:47 +0900

    공통 모듈 분리

---
+++ js/common.js
@@ -0,0 +1,20 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function() {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+  //html의 속성을 지정할 때 setAttribute를 사용한다. 
+});
+
+searchInputEl.addEventListener('blur', function() {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
